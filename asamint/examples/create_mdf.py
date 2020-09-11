e63687c0ff1fc51f6e8287bdab97ccb14462df32
Christoph Schueler <cpu12.gems@googlemail.com>
2020-09-11 09:14:20 +0200

    Update create_mdf.py

--- asamint/examples/create_mdf.py
+++ asamint/examples/create_mdf.py
@@ -11,9 +11,9 @@ from asamint.config import readConfiguration
 #from pya2l import DB
 
 mdf = MDF(version = "4.10")
-create_mdf(session_obj = session, mdf_obj = mdf, mdf_filename = "ASAP2_Demo_V161.mf4")
 
 project_config = readConfiguration("example.apj")
 experiment_config = readConfiguration("first_experiment.epj")
+
 
 mxx = MDFCreator(mdf_obj = mdf, project_config = project_config, experiment_config = experiment_config)
