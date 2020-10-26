d3ee6afef5531169d80204ec0f465d89abdea1d2
Christoph Schueler <cpu12.gems@googlemail.com>
2020-10-26 09:17:18 +0100

    Create sample HEX file if it doesn't exists

--- asamint/examples/blueparrot/flash_emulator/write_flash.py
+++ asamint/examples/blueparrot/flash_emulator/write_flash.py
@@ -26,13 +26,17 @@ __copyright__ = """
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-import time
+from os.path import exists
 
+from create import create_example_hexfile
 
 from objutils import load
 from pyxcp.cmdline import ArgumentParser
 
 HEX_FILE_NAME = "paged_flash.s28"
+
+if not exists(HEX_FILE_NAME):
+     create_example_hexfile()
 
 hex_file = load("srec", HEX_FILE_NAME)
 
