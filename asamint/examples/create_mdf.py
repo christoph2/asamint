5c5f7aef9c137f57eccee1a9f2d8d687f2b869fe
Christoph Schueler <cpu12.gems@googlemail.com>
2020-11-10 05:03:10 +0100

    Fix create_mdf.py

--- asamint/examples/create_mdf.py
+++ asamint/examples/create_mdf.py
@@ -7,12 +7,11 @@
 import toml
 
 from asamint.mdf import MDFCreator
-from asamint.config import readConfiguration
 
 from asamint.utils import generate_filename
 
 project_config = toml.load("example.apr")
-experiment_config = toml.load("first_experiment.epj")
+experiment_config = toml.load("first_experiment.exp")
 print(project_config, end = "\n\n")
 print(experiment_config, end = "\n\n")
 
