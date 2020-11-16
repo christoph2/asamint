3a87bade868c801789fe07b05daf4f2e68f80c46
Christoph Schueler <cpu12.gems@googlemail.com>
2020-11-16 06:59:07 +0100

    create_mdf() as a method

--- asamint/mdf/__init__.py
+++ asamint/mdf/__init__.py
@@ -109,88 +109,88 @@ class MDFCreator(AsamBaseType):
         pass
 
 
-def create_mdf(session_obj, mdf_obj, mdf_filename = None):
-    """
-    Parameters
-    ----------
-    """
+    def create_mdf(self, mdf_obj, mdf_filename = None):
+        """
+        Parameters
+        ----------
+        """
 
-    signals = []
-    measurements = session_obj.query(model.Measurement).order_by(model.Measurement.name).all()
-    for measurement in measurements:
-        cm_name = measurement.conversion
-        comment = measurement.longIdentifier
-        unit = None
-        data_type = measurement.datatype # Don't know how to set data types on Signals...
-        conversion = None
-        if cm_name == "NO_COMPU_METHOD":
+        signals = []
+        measurements = self.query(model.Measurement).order_by(model.Measurement.name).all()
+        for measurement in measurements:
+            cm_name = measurement.conversion
+            comment = measurement.longIdentifier
+            unit = None
+            data_type = measurement.datatype # Don't know how to set data types on Signals...
             conversion = None
-        else:
-            cm = session_obj.query(model.CompuMethod).filter(model.CompuMethod.name ==  cm_name).first()
-            cm_type = cm.conversionType
-            unit = cm.unit
-            if cm_type == "IDENTICAL":
+            if cm_name == "NO_COMPU_METHOD":
                 conversion = None
-            elif cm_type == "FORM":
-                formula_inv = cm.formula.formula_inv.g_x if cm.formula.formula_inv else None
-                conversion = {
-                    "formula": cm.formula.f_x
-                }
-            elif cm_type == "LINEAR":
-                conversion = {
-                    "a": cm.coeffs_linear.a,
-                    "b": cm.coeffs_linear.b,
-                }
-            elif cm_type == "RAT_FUNC":
-                conversion = {
-                    "P1": cm.coeffs.a,
-                    "P2": cm.coeffs.b,
-                    "P3": cm.coeffs.c,
-                    "P4": cm.coeffs.d,
-                    "P5": cm.coeffs.e,
-                    "P6": cm.coeffs.f,
-                }
-            elif cm_type in ("TAB_INTP", "TAB_NOINTP"):
-                interpolation = True if cm_type == "TAB_INTP" else False
-                cvt = session_obj.query(model.CompuTab).filter(model.CompuTab.name == cm.compu_tab_ref.conversionTable).first()
-                pairs = cvt.pairs
-                num_values = len(pairs)
-                default_value = cvt.default_value_numeric.display_value if cvt.default_value_numeric else None
-                #print("\tTAB_INTP", measurement.name, cvt.pairs, default_value)
-                in_values = [x.inVal for x in pairs]
-                out_values = [x.outVal for x in pairs]
-                conversion = {"raw_{}".format(i): in_values[i] for i in range(num_values)}
-                conversion.update({"phys_{}".format(i): out_values[i] for i in range(num_values)})
-                conversion.update(default = default_value)
-                conversion.update(interpolation = interpolation)
-            elif cm_type == "TAB_VERB":
-                cvt = session_obj.query(model.CompuVtab).filter(model.CompuVtab.name == cm.compu_tab_ref.conversionTable).first()
-                if cvt:
+            else:
+                cm = self.query(model.CompuMethod).filter(model.CompuMethod.name ==  cm_name).first()
+                cm_type = cm.conversionType
+                unit = cm.unit
+                if cm_type == "IDENTICAL":
+                    conversion = None
+                elif cm_type == "FORM":
+                    formula_inv = cm.formula.formula_inv.g_x if cm.formula.formula_inv else None
+                    conversion = {
+                        "formula": cm.formula.f_x
+                    }
+                elif cm_type == "LINEAR":
+                    conversion = {
+                        "a": cm.coeffs_linear.a,
+                        "b": cm.coeffs_linear.b,
+                    }
+                elif cm_type == "RAT_FUNC":
+                    conversion = {
+                        "P1": cm.coeffs.a,
+                        "P2": cm.coeffs.b,
+                        "P3": cm.coeffs.c,
+                        "P4": cm.coeffs.d,
+                        "P5": cm.coeffs.e,
+                        "P6": cm.coeffs.f,
+                    }
+                elif cm_type in ("TAB_INTP", "TAB_NOINTP"):
+                    interpolation = True if cm_type == "TAB_INTP" else False
+                    cvt = session_obj.query(model.CompuTab).filter(model.CompuTab.name == cm.compu_tab_ref.conversionTable).first()
                     pairs = cvt.pairs
                     num_values = len(pairs)
+                    default_value = cvt.default_value_numeric.display_value if cvt.default_value_numeric else None
+                    #print("\tTAB_INTP", measurement.name, cvt.pairs, default_value)
                     in_values = [x.inVal for x in pairs]
                     out_values = [x.outVal for x in pairs]
-                    conversion = {"val_{}".format(i): in_values[i] for i in range(num_values)}
-                    conversion.update({"text_{}".format(i): out_values[i] for i in range(num_values)})
-                    conversion.update(default = cvt.default_value.display_string if cvt.default_value else None)
-                else:
-                    cvt = session_obj.query(model.CompuVtabRange).filter(model.CompuVtabRange.name == \
-                            cm.compu_tab_ref.conversionTable).first()
+                    conversion = {"raw_{}".format(i): in_values[i] for i in range(num_values)}
+                    conversion.update({"phys_{}".format(i): out_values[i] for i in range(num_values)})
+                    conversion.update(default = default_value)
+                    conversion.update(interpolation = interpolation)
+                elif cm_type == "TAB_VERB":
+                    cvt = session_obj.query(model.CompuVtab).filter(model.CompuVtab.name == cm.compu_tab_ref.conversionTable).first()
                     if cvt:
-                        triples = cvt.triples
-                        num_values = len(triples)
-                        lower_values = [x.inValMin for x in triples]
-                        upper_values = [x.inValMax for x in triples]
-                        text_values = [x.outVal for x in triples]
-                        conversion = {"lower_{}".format(i): lower_values[i] for i in range(num_values)}
-                        conversion.update({"upper_{}".format(i): upper_values[i] for i in range(num_values)})
-                        conversion.update({"text_{}".format(i): text_values[i] for i in range(num_values)})
-                        conversion.update(default = bytes(cvt.default_value.display_string, encoding = "utf-8") \
-                                if cvt.default_value else b'')
+                        pairs = cvt.pairs
+                        num_values = len(pairs)
+                        in_values = [x.inVal for x in pairs]
+                        out_values = [x.outVal for x in pairs]
+                        conversion = {"val_{}".format(i): in_values[i] for i in range(num_values)}
+                        conversion.update({"text_{}".format(i): out_values[i] for i in range(num_values)})
+                        conversion.update(default = cvt.default_value.display_string if cvt.default_value else None)
                     else:
-                        conversion = None
-        #print(measurement.name, conversion)
-        signal = Signal(samples = [0, 0, 0, 0], timestamps = [0, 0, 0, 0], name = measurement.name, unit = unit, conversion = conversion, comment = comment)
-        signals.append(signal)
-    mdf_obj.append(signals)
-    mdf_obj.save(dst = mdf_filename, overwrite = True)
+                        cvt = session_obj.query(model.CompuVtabRange).filter(model.CompuVtabRange.name == \
+                                cm.compu_tab_ref.conversionTable).first()
+                        if cvt:
+                            triples = cvt.triples
+                            num_values = len(triples)
+                            lower_values = [x.inValMin for x in triples]
+                            upper_values = [x.inValMax for x in triples]
+                            text_values = [x.outVal for x in triples]
+                            conversion = {"lower_{}".format(i): lower_values[i] for i in range(num_values)}
+                            conversion.update({"upper_{}".format(i): upper_values[i] for i in range(num_values)})
+                            conversion.update({"text_{}".format(i): text_values[i] for i in range(num_values)})
+                            conversion.update(default = bytes(cvt.default_value.display_string, encoding = "utf-8") \
+                                    if cvt.default_value else b'')
+                        else:
+                            conversion = None
+            #print(measurement.name, conversion)
+            signal = Signal(samples = [0, 0, 0, 0], timestamps = [0, 0, 0, 0], name = measurement.name, unit = unit, conversion = conversion, comment = comment)
+            signals.append(signal)
+        mdf_obj.append(signals)
+        mdf_obj.save(dst = mdf_filename, overwrite = True)
