6f07157c5bf6a95b88e280ecb2b03b79eb7b18b0
Christoph Schueler <cpu12.gems@googlemail.com>
2020-11-21 10:08:20 +0100

    Init gh_pages branch

---
+++ content/code_rev.rst
@@ -0,0 +1,18 @@
+My first code review
+####################
+
+:category: code_review
+:tags: xcp; automotive; code
+:date: 2020-10-27 10:20
+:authors: Christoph Schueler
+
+.. code-block:: python
+
+    if __name__ == "__main__":
+        main()
+
+
+This will be turned into :abbr:`HTML (HyperText Markup Language)`.
+
+
+
---
+++ pelicanconf.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Christoph Schueler'
+SITENAME = 'ASAM Intergration Package Newsfeed'
+SITEURL = ''
+#SITESUBTITLE = 'Site Subtitle'
+SITEDESCRIPTION = "Foo Bar's Thoughts and Writings"
+#SITELOGO = SITEURL + "/images/profile.png"
+#FAVICON = SITEURL + "/images/favicon.ico"
+
+BROWSER_COLOR = "#333"
+ROBOTS = "index, follow"
+
+PATH = 'content'
+TIMEZONE = 'Europe/Berlin'
+DEFAULT_LANG = 'en'
+DEFAULT_DATE = "fs"
+
+COPYRIGHT_YEAR = 2020
+
+#EXTRA_PATH_METADATA = {
+#    "extra/custom.css": {"path": "static/custom.css"},
+#}
+
+#CUSTOM_CSS = "static/custom.css"
+MAIN_MENU = True
+
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = ''
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    #('Pelican', 'https://getpelican.com/'),
+    #('Python.org', 'https://www.python.org/'),
+    #('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+    #('You can modify those links in your config file', '#'),
+)
+
+# Social widget
+SOCIAL = (
+    ('github', 'https://github.com/christoph2'),
+#    ('You can add links in _your config file', '#'),
+#    ('Another social link', '#'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+FILENAME_METADATA = '(?P<title>. * )'
+LOAD_CONTENT_CACHE = False
+SUMMARY_MAX_LENGTH = 60
+DEFAULT_PAGINATION = 10
+GITHUB_URL = 'https://github.com/christoph2'
+
+THEME = 'themes/Flex'
+
+# DISQUS_SITENAME = ''
+# GOOGLE_ANALYTICS = ''
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
