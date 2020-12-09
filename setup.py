ce4a9c5d04e771091e1c6942c949f33f1ad294e8
Christoph Schueler <cpu12.gems@googlemail.com>
2020-12-09 11:22:21 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ["asammdf", "objutils", "pya2ldb", "pyxcp"]
 
 setup_requirements = ['pytest-runner', ]
 
