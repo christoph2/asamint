764f230e1a00322fc34f4462e4b28f2e9b747829
Christoph Schueler <cpu12.gems@googlemail.com>
2021-02-02 13:57:29 +0100

    Add lz requirement

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["asammdf", "objutils", "pya2ldb", "pyxcp", "babel"]
+requirements = ["asammdf", "objutils", "pya2ldb", "pyxcp", "babel", "lz4"]
 
 setup_requirements = ['pytest-runner', ]
 
