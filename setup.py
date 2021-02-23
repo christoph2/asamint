6e76154a8994c88c0ea4b89a6a71f751fc3fc1be
Christoph Schueler <cpu12.gems@googlemail.com>
2021-02-23 06:52:50 +0100

    Require sortedcontainers

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('docs/README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["asammdf", "objutils", "pya2ldb", "pyxcp", "babel", "lz4"]
+requirements = ["asammdf", "objutils", "pya2ldb", "pyxcp", "babel", "lz4", "sortedcontainers"]
 
 setup_requirements = ['pytest-runner', ]
 
