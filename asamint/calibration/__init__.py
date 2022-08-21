8079e7dfa7ccbca8548ef6f65ae5f20dd4ad28b9
Christoph Schueler <cpu12.gems@googlemail.com>
2022-08-21 16:57:38 +0200

    load_value() and save_value()

--- asamint/calibration/__init__.py
+++ asamint/calibration/__init__.py
@@ -29,21 +29,24 @@ __copyright__ = """
 """
 
 from collections import OrderedDict
+from enum import IntEnum
 import functools
 import os
+from typing import Any, Union
 
 import numpy as np
+from pya2l.classes import READ_ONLY
 
 from asamint.asam import AsamBaseType
 
-from asamint.asam import TYPE_SIZES, get_section_reader
+from asamint.asam import ByteOrder, TYPE_SIZES, get_section_reader
 from asamint.logger import Logger
 from asamint.utils import SINGLE_BITS, ffs, current_timestamp
 from asamint.utils.optimize import McObject, make_continuous_blocks
 from asamint.calibration import model as cmod
 
 from pya2l import DB
-from pya2l.api.inspect import AxisPts, CompuMethod, Characteristic, ModPar
+from pya2l.api.inspect import AxisPts, CompuMethod, Characteristic, ModCommon, ModPar
 from pya2l.api import inspect
 from pya2l.functions import fix_axis_par, fix_axis_par_dist
 
@@ -51,7 +54,55 @@ import pya2l.model as model
 
 from objutils import dump, load, Image, Section
 
+
+ValueType = Union[float, int, bool, str]
+BOOLEAN_MAP = {"true": 1, "false": 0}
 AXES = ("x", "y", "z", "4", "5")
+
+
+class ExecutionPolicy(IntEnum):
+    EXCEPT = 0
+    RETURN_ERROR = 1
+    IGNORE = 2
+
+
+class Status(IntEnum):
+    OK = 0
+    READ_ONLY_ERROR = 1
+    RANGE_ERROR = 2
+
+
+class RangeError(Exception):
+    pass
+
+
+class ReadOnlyError(Exception):
+    pass
+
+
+def check_limits(characteristic, value: float, extended_limits: bool = False) -> bool:
+    """
+    Check if the value is within the limits of the characteristic.
+
+    Parameters
+    ----------
+    value: float
+        Value to check
+
+    extended_limits: bool
+        Also check extended limits if available
+
+    Returns
+    -------
+    bool
+    """
+    if value < characteristic.lowerLimit or value > characteristic.upperLimit:
+        return False
+    if extended_limits and characteristic.extendedLimits:
+        limits = characteristic.extendedLimits
+        if value < limits.get("lowerLimit") or value > limits.get("upperLimit"):
+            return False
+    return True
 
 
 class Calibration:
@@ -60,21 +111,17 @@ class Calibration:
     def __init__(self, a2l_db: DB, image, loglevel: str = "WARN") -> None:
         self.image = image
         self.a2l_db = a2l_db
+        self.session = self.a2l_db.session
         self.logger = Logger(loglevel)
+        self.mod_common = ModCommon.get(self.session)
+        self.mod_par = ModPar.get(self.session) if ModPar.exists(self.session) else None
 
     def update(self):
         """To the actual update of parameters (write to HEX file / XCP)."""
         pass
 
-    def int_to_physical(self, characteristic, int_values):
-        """ """
-        cm_name = "NO_COMPU_METHOD" if characteristic.compuMethod == "NO_COMPU_METHOD" else characteristic.compuMethod.name
-        cm = CompuMethod.get(self.a2l_db.session, cm_name)
-        return cm.int_to_physical(int_values)
-
     def load_ascii(self, characteristic_name: str) -> cmod.Ascii:
-        characteristic = self._load_characteristic(characteristic_name, "ASCII")
-        self.logger.debug(f"Loading ASCII '{characteristic.name}' @0x{characteristic.address:08x}")
+        characteristic = self.get_characteristic(characteristic_name, "ASCII", False)
         if characteristic.matrixDim:
             length = characteristic.matrixDim["x"]
         else:
@@ -89,18 +136,23 @@ class Calibration:
             length=length,
         )
 
-    def save_ascii(self, characteristic_name: str, value: str) -> None:
-        characteristic = self._load_characteristic(characteristic_name, "ASCII")
-        self.logger.debug(f"Saving ASCII '{characteristic.name}' @0x{characteristic.address:08x}")
+    def save_ascii(self, characteristic_name: str, value: str, readOnlyPolicy: ExecutionPolicy = ExecutionPolicy.EXCEPT) -> Status:
+        characteristic = self.get_characteristic(characteristic_name, "ASCII", True)
+        if characteristic.readOnly:
+            self.logger.info(f"Characteristic '{characteristic_name}' is READ-ONLY!")
+            if readOnlyPolicy == ExecutionPolicy.EXCEPT:
+                raise ReadOnlyError(f"Characteristic '{characteristic_name}' is read-only.")
+            elif readOnlyPolicy == ExecutionPolicy.RETURN_ERROR:
+                return Status.READ_ONLY_ERROR
         if characteristic.matrixDim:
             length = characteristic.matrixDim["x"]
         else:
             length = characteristic.number
         self.image.write_string(characteristic.address, length=length, value=value)
+        return Status.OK
 
     def load_value_block(self, characteristic_name: str) -> cmod.ValueBlock:
-        characteristic = self._load_characteristic(characteristic_name, "VAL_BLK")
-        self.logger.debug("Loading VAL_BLK '{}' @0x{:08x}".format(characteristic.name, characteristic.address))
+        characteristic = self.get_characteristic(characteristic_name, "VAL_BLK", False)
         reader = get_section_reader("UBYTE", 0)
         raw_values = self.image.read_ndarray(
             addr=characteristic.address,
@@ -122,9 +174,191 @@ class Calibration:
             unit=characteristic.physUnit,
         )
 
-    def save_value_block(self, characteristic_name: str, value: str) -> None:
-        characteristic = self._load_characteristic(characteristic_name, "ASCII")
-        self.logger.debug(f"Saving VAL_BLK '{characteristic.name}' @0x{characteristic.address:08x}")
+    def save_value_block(
+        self, characteristic_name: str, values: np.ndarray, readOnlyPolicy: ExecutionPolicy = ExecutionPolicy.EXCEPT
+    ) -> Status:
+        characteristic = self.get_characteristic(characteristic_name, "VAL_BLK", True)
+        if characteristic.readOnly:
+            self.logger.info(f"Characteristic '{characteristic_name}' is READ-ONLY!")
+            if readOnlyPolicy == ExecutionPolicy.EXCEPT:
+                raise ReadOnlyError(f"Characteristic '{characteristic_name}' is read-only.")
+            elif readOnlyPolicy == ExecutionPolicy.RETURN_ERROR:
+                return Status.READ_ONLY_ERROR
+        values = self.physical_to_int(characteristic, values)
+        self.image.write_ndarray(addr=characteristic.address, array=values, order=characteristic.fnc_np_order)
+        return Status.OK
+
+    def load_value(self, characteristic_name: str) -> cmod.Value:
+        characteristic = self.get_characteristic(characteristic_name, "VALUE", False)
+
+        # CALIBRATION_ACCESS
+        fnc_asam_dtype = characteristic.fnc_asam_dtype
+        reader = get_section_reader(fnc_asam_dtype, self.byte_order(characteristic))
+        if characteristic.bitMask:
+            raw_value = self.image.read_numeric(characteristic.address, reader, bit_mask=characteristic.bitMask)
+            raw_value >>= ffs(characteristic.bitMask)  # Right-shift to get rid of trailing zeros (s. ASAM 2-MC spec).
+            is_bool = True if characteristic.bitMask in SINGLE_BITS else False
+        else:
+            raw_value = self.image.read_numeric(characteristic.address, reader)
+            is_bool = False
+        if characteristic.physUnit is None and characteristic._conversionRef != "NO_COMPU_METHOD":
+            unit = characteristic.compuMethod.unit
+        else:
+            unit = characteristic.physUnit
+        converted_value = self.int_to_physical(characteristic, raw_value)
+        if isinstance(converted_value, (int, float)):
+            if is_bool:
+                category = "BOOLEAN"
+                converted_value = "true" if bool(converted_value) else "false"
+            else:
+                category = "VALUE"
+        else:
+            category = "TEXT"
+        if characteristic.dependentCharacteristic:
+            category = "DEPENDENT_VALUE"
+        return cmod.Value(
+            name=characteristic.name,
+            comment=characteristic.longIdentifier,
+            category=category,
+            raw_value=raw_value,
+            converted_value=converted_value,
+            displayIdentifier=characteristic.displayIdentifier,
+            unit=unit,
+        )
+
+    def save_value(
+        self,
+        characteristic_name: str,
+        value: ValueType,
+        extendedLimits: bool = False,
+        readOnlyPolicy: ExecutionPolicy = ExecutionPolicy.EXCEPT,
+        limitsPolicy: ExecutionPolicy = ExecutionPolicy.EXCEPT,
+    ) -> Status:
+        if not isinstance(value, ValueType):
+            raise TypeError("value must be float, int, bool or str")
+        characteristic = self.get_characteristic(characteristic_name, "VALUE", True)
+        if characteristic.readOnly:
+            self.logger.info(f"Characteristic '{characteristic_name}' is READ-ONLY!")
+            if readOnlyPolicy == ExecutionPolicy.EXCEPT:
+                raise ReadOnlyError(f"Characteristic '{characteristic_name}' is read-only.")
+            elif readOnlyPolicy == ExecutionPolicy.RETURN_ERROR:
+                return Status.READ_ONLY_ERROR
+        if characteristic.compuMethod.conversionType == "TAB_VERB":
+            text_values = characteristic.compuMethod.tab_verb.get("text_values")
+            if value not in text_values:
+                raise ValueError(f"value must be in {text_values} got {value}.")
+        else:
+            if isinstance(value, bool):
+                value = int(value)
+            elif isinstance(value, str):
+                pass
+                if value in ("true", "false"):
+                    value = BOOLEAN_MAP[value]
+                else:
+                    raise ValueError("value of type str must be 'true' or 'false'")
+        dtype = get_section_reader(characteristic.fnc_asam_dtype, self.byte_order(characteristic))
+
+        if isinstance(value, (int, float)) and not check_limits(characteristic, value, extendedLimits):
+            self.logger.info(f"Characteristic '{characteristic_name}' is out of range")
+            if limitsPolicy == ExecutionPolicy.EXCEPT:
+                raise RangeError(f"Characteristic '{characteristic_name}' is out of range")
+            elif limitsPolicy == ExecutionPolicy.RETURN_ERROR:
+                return Status.RANGE_ERROR
+
+        converted_value = self.physical_to_int(characteristic, value)
+        if characteristic.bitMask:
+            converted_value = int(converted_value)
+            converted_value <<= ffs(characteristic.bitMask)
+        self.image.write_numeric(characteristic.address, converted_value, dtype)
+        return Status.OK
+
+    def load_axis_pts(self, axis_pts_name: str) -> Status:
+        ap = self.get_axis_pts(axis_pts_name, False)
+        # ap = AxisPts.get(self.session, axis_pts_name)
+        # mem_size = ap.total_allocated_memory
+        self.logger.debug("Processing AXIS_PTS '{}' @0x{:08x}".format(ap.name, ap.address))
+        rl_values = self.read_record_layout_values(ap, "x")
+        self.record_layout_correct_offsets(ap)
+        virtual = False
+        axis = ap.record_layout_components.axes("x")
+        paired = False
+        if "axisRescale" in axis:
+            category = "RES_AXIS"
+            paired = True
+            if "noRescale" in rl_values:
+                no_rescale_pairs = rl_values["noRescale"]
+            else:
+                no_rescale_pairs = axis["maxNumberOfRescalePairs"]
+            index_incr = axis["axisRescale"]["indexIncr"]
+            count = no_rescale_pairs * 2
+            attr = "axisRescale"
+        elif "axisPts" in axis:
+            category = "COM_AXIS"
+            if "noAxisPts" in rl_values:
+                no_axis_points = rl_values["noAxisPts"]
+            else:
+                no_axis_points = axis["maxAxisPoints"]
+            index_incr = axis["axisPts"]["indexIncr"]
+            count = no_axis_points
+            attr = "axisPts"
+        elif "offset" in axis:
+            category = "FIX_AXIS"
+            virtual = True  # Virtual / Calculated axis.
+            offset = rl_values.get("offset")
+            dist_op = rl_values.get("distOp")
+            shift_op = rl_values.get("shiftOp")
+            if "noAxisPts" in rl_values:
+                no_axis_points = rl_values["noAxisPts"]
+            else:
+                no_axis_points = axis["maxAxisPoints"]
+            if (dist_op or shift_op) is None:
+                raise TypeError("Malformed AXIS_PTS '{}', neither DIST_OP nor SHIFT_OP specified.".format(ap))
+            if dist_op is not None:
+                raw_values = fix_axis_par_dist(offset, dist_op, no_axis_points)
+            else:
+                raw_values = fix_axis_par(offset, shift_op, no_axis_points)
+        else:
+            raise TypeError("Malformed AXIS_PTS '{}'.".format(ap))
+        if not virtual:
+            raw_values = self.read_nd_array(ap, "x", attr, count)
+            if index_incr == "INDEX_DECR":
+                raw_values = raw_values[::-1]
+                reversed_storage = True
+            else:
+                reversed_storage = False
+        converted_values = self.int_to_physical(ap, raw_values)
+        unit = ap.compuMethod.refUnit
+        return cmod.AxisPts(
+            name=ap.name,
+            comment=ap.longIdentifier,
+            category=category,
+            raw_values=raw_values,
+            converted_values=converted_values,
+            displayIdentifier=ap.displayIdentifier,
+            paired=paired,
+            unit=unit,
+            reversed_storage=reversed_storage,
+        )
+
+    def int_to_physical(self, characteristic, int_values):
+        """Convert ECU internal values to physical representation."""
+        cm = self.get_compu_method(characteristic)
+        return cm.int_to_physical(int_values)
+
+    def physical_to_int(self, characteristic, physical_values):
+        """Convert physical values to ECU internal representation."""
+        cm = self.get_compu_method(characteristic)
+        return cm.physical_to_int(physical_values)
+
+    def get_compu_method(self, characteristic):
+        cm_name = "NO_COMPU_METHOD" if characteristic.compuMethod == "NO_COMPU_METHOD" else characteristic.compuMethod.name
+        return CompuMethod.get(self.a2l_db.session, cm_name)
+
+    def get_characteristic(self, characteristic_name, type_name: str, save: bool = False):
+        characteristic = self._load_characteristic(characteristic_name, type_name)
+        direction = "Saving" if save else "Loading"
+        self.logger.debug(f"{direction} {type_name} '{characteristic.name}' @0x{characteristic.address:08x}")
+        return characteristic
 
     def _load_characteristic(self, characteristic_name, category):
         try:
@@ -137,6 +371,132 @@ class Calibration:
 
     def _save_characteristic(self, characteristic, value):
         pass
+
+    def get_axis_pts(self, axis_pts_name, save: bool = False):
+        axis_pts = self._load_axis_pts(axis_pts_name)
+        direction = "Saving" if save else "Loading"
+        self.logger.debug(f"{direction} AXIS_PTS '{axis_pts.name}' @0x{axis_pts.address:08x}")
+        return axis_pts
+
+    def _load_axis_pts(self, axis_pts_name):
+        try:
+            axis_pts = AxisPts.get(self.a2l_db.session, axis_pts_name)
+        except ValueError:
+            raise
+        return axis_pts
+
+    def byte_order(self, obj):
+        """Get byte-order for A2L element.
+
+        Parameters
+        ----------
+        obj: (`AxisPts` | `AxisDescr` | `Measurement` | `Characteristic`) instance.
+
+        Returns
+        -------
+        `ByteOrder`:
+            If element has no BYTE_ORDER, lookup MOD_COMMON else ByteOrder.BIG_ENDIAN
+        """
+        return (
+            ByteOrder.BIG_ENDIAN
+            if obj.byteOrder or self.mod_common.byteOrder in ("MSB_FIRST", "LITTLE_ENDIAN")
+            else ByteOrder.LITTLE_ENDIAN
+        )
+
+    def record_layout_correct_offsets(self, obj):
+        """ """
+        no_axis_pts = {}
+        no_rescale = {}
+        axis_pts = {}
+        axis_rescale = {}
+        # fnc_values = None
+        for _, component in obj.record_layout_components:
+            component_type = component["type"]
+            if component_type == "fncValues":
+                # fnc_values = component
+                pass
+            elif len(component_type) == 2:
+                name, axis_name = component_type
+                if name == "noAxisPts":
+                    no_axis_pts[axis_name] = self.read_record_layout_value(obj, axis_name, name)
+                elif name == "noRescale":
+                    no_rescale[axis_name] = self.read_record_layout_value(obj, axis_name, name)
+                elif name == "axisPts":
+                    axis_pts[axis_name] = component
+                elif name == "axisRescale":
+                    axis_rescale[axis_name] = component
+            else:
+                pass
+        biases = {}
+        for key, value in no_axis_pts.items():
+            axis_pt = axis_pts.get(key)
+            if axis_pt:
+                max_axis_points = axis_pt["maxAxisPoints"]
+                bias = value - max_axis_points
+                biases[axis_pt["position"] + 1] = bias
+        for key, value in no_rescale.items():
+            axis_rescale = axis_rescale.get(key)
+            if axis_rescale:
+                max_number_of_rescale_pairs = axis_rescale["maxNumberOfRescalePairs"]
+                bias = value - max_number_of_rescale_pairs
+                biases[axis_rescale["position"] + 1] = bias
+        total_bias = 0
+        if biases:
+            for pos, component in obj.record_layout_components:
+                if pos in biases:
+                    bias = biases.get(pos)
+                    total_bias += bias
+                component["offset"] += total_bias
+
+    def read_record_layout_values(self, obj, axis_name):
+        DATA_POINTS = (
+            "distOp",
+            "identification",
+            "noAxisPts",
+            "noRescale",
+            "offset",
+            "reserved",
+            "ripAddr",
+            "srcAddr",
+            "shiftOp",
+        )
+        result = {}
+        axis = obj.record_layout_components.axes(axis_name)
+        for key in DATA_POINTS:
+            if key in axis:
+                result[key] = self.read_record_layout_value(obj, axis_name, key)
+        return result
+
+    def read_record_layout_value(self, obj, axis_name, component_name):
+        """ """
+        axis = obj.record_layout_components.axes(axis_name)
+        component_map = axis.get(component_name)
+        if component_map is None:
+            return None
+        datatype = component_map["datatype"]
+        offset = component_map["offset"]
+        reader = get_section_reader(datatype, self.byte_order(obj))
+        value = self.image.read_numeric(obj.address + offset, reader)
+        return value
+
+    def read_nd_array(self, axis_pts, axis_name, component, no_elements, shape=None, order=None):
+        """ """
+        axis = axis_pts.record_layout_components.axes(axis_name)
+        component_map = axis[component]
+        datatype = component_map["datatype"]
+        offset = component_map["offset"]
+        reader = get_section_reader(datatype, self.byte_order(axis_pts))
+
+        length = no_elements * TYPE_SIZES[datatype]
+        np_arr = self.image.read_ndarray(
+            addr=axis_pts.address + offset,
+            length=length,
+            dtype=reader,
+            shape=shape,
+            order=order,
+            # bit_mask = characteristic.bitMask
+        )
+        return np_arr
 
 
 class OnlineCalibration(Calibration):
--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -7,6 +7,27 @@ import numpy as np
 import pytest
 
 from asamint import calibration
+from asamint.calibration.model import Value
+
+
+class Boolean:
+    def __init__(self, value: bool = False) -> None:
+        self.value = value
+
+    def __bool__(self) -> bool:
+        return self.value
+
+    def __str__(self) -> str:
+        return "true" if self.value else "false"
+
+    __repr__ = __str__
+    # def __repr__(self):
+    #    return f"bool({self.value})"
+
+
+def test_boolean():
+    b = Boolean(True)
+    assert str(b) == "true"
 
 
 @pytest.fixture
@@ -51,3 +72,212 @@ def test_block(offline):
     #    [0.69582202,  0.80568865, 0.90334788],
     #    [1.00100711,  1.09866634, 1.19632557]])
     # ))
+    offline.save_value_block("CDF20.MATRIX_DIM.N341_wS8", block.converted_values)
+
+
+def load_save_verify_value(conn, param_name, expected_rw, expected_cw):
+    value = conn.load_value(param_name)
+    assert value.converted_value == expected_cw
+    assert value.raw_value == expected_rw
+    conn.save_value(param_name, value.converted_value)
+
+
+def test_value_uword(offline):
+    load_save_verify_value(offline, "CDF20.scalar.FW_wU16", 34, 0.4150517288735618)
+
+
+def test_value_dependent(offline):
+    load_save_verify_value(offline, "CDF20.Dependent.Base.FW_wU16", 17, 17.0)
+
+
+def test_value_bool1(offline):
+    load_save_verify_value(offline, "CDF20.BOOLEAN.FW_wU8", 1, "true")
+    # load_save_verify_value(offline, "CDF20.BOOLEAN.FW_wU8", 1, True)
+    # load_save_verify_value(offline, "CDF20.BOOLEAN.FW_wU8", 1, 1)
+
+
+def test_value_bool2(offline):
+    load_save_verify_value(offline, "CDF20.BOOLEAN.FW_wU8_VTab", 1, "TRUE")
+
+
+def test_value001(offline):
+    load_save_verify_value(offline, "CDF20", 65535, 65535.0)
+
+
+def test_value002(offline):
+    with pytest.raises(calibration.ReadOnlyError):
+        load_save_verify_value(offline, "CDF20.Dependent.Ref_1.FW_wU16", 85, 85.0)
+
+
+def test_value003(offline):
+    load_save_verify_value(offline, "CDF20.array.Element[0]", 248, 0.2421875)
+
+
+def test_value004(offline):
+    load_save_verify_value(offline, "CDF20.array.Element[1]", 16, 0.015625)
+
+
+def test_value005(offline):
+    load_save_verify_value(offline, "CDF20.array.Element[2]", 28, 0.02734375)
+
+
+def test_value006(offline):
+    load_save_verify_value(offline, "CDF20.array.Element[3]", 40, 0.0390625)
+
+
+def test_value007(offline):
+    load_save_verify_value(offline, "CDF20.array.Element[4]", 80, 0.078125)
+
+
+def test_value008(offline):
+    load_save_verify_value(offline, "CDF20.scalar.FW_wU16", 34, 0.4150517288735618)
+
+
+def test_value009(offline):
+    load_save_verify_value(offline, "DummyAirMass", 2560, 1.25)
+
+
+def test_value010(offline):
+    load_save_verify_value(offline, "DummyOmega", 32770, 1024.0625)
+
+
+def test_value011(offline):
+    load_save_verify_value(offline, "FrequencyPrescaler", 2560, 0.256)
+
+
+def test_value012(offline):
+    load_save_verify_value(offline, "SelectColumn", 64, 0.001953125)
+
+
+def test_value013(offline):
+    load_save_verify_value(offline, "Select_param_set", 1, 1.0)
+
+
+def test_value014(offline):
+    with pytest.raises(calibration.RangeError):
+        load_save_verify_value(offline, "SignalAmplitude", -32746, -255.828125)
+
+
+def test_value015(offline):
+    load_save_verify_value(offline, "SignalForm", 3, "Sin")
+
+
+def test_value016(offline):
+    load_save_verify_value(offline, "SignalOffset", 25, 0.1953125)
+
+
+def test_value017(offline):
+    load_save_verify_value(offline, "Soll_Lambda", 128, 1.0)
+
+
+def test_value018(offline):
+    load_save_verify_value(offline, "ThrottleRefInputTest", 19456, 152.0)
+
+
+def test_value019(offline):
+    load_save_verify_value(offline, "f_Kd_1", 25104, 0.005985260009765625)
+
+
+def test_value020(offline):
+    load_save_verify_value(offline, "f_Kd_2", 25104, 0.005985260009765625)
+
+
+def test_value021(offline):
+    load_save_verify_value(offline, "f_Ki_1", 16, 0.00390625)
+
+
+def test_value022(offline):
+    load_save_verify_value(offline, "f_Ki_2", 48, 0.01171875)
+
+
+def test_value023(offline):
+    load_save_verify_value(offline, "f_Kp_1", 52236, 1.5941162109375)
+
+
+def test_value024(offline):
+    load_save_verify_value(offline, "f_Kp_2", 26150, 0.79803466796875)
+
+
+def test_value025(offline):
+    load_save_verify_value(offline, "inj_offset", -4074, -0.0004856586456298828)
+
+
+def test_value026(offline):
+    load_save_verify_value(offline, "inv_c_kumsrl", -29127, -1820.4375)
+
+
+def load_save_verify_axis_pts(conn, param_name, expected_rw, expected_cw):
+    value = conn.load_axis_pts(param_name)
+    assert np.array_equal(value.converted_values, expected_cw)
+    assert np.array_equal(value.raw_values, expected_rw)
+    # conn.save_axis_pts(param_name, value.converted_value)
+
+
+def test_axis_pts001(offline):
+    load_save_verify_axis_pts(offline, "CDF20.axis.X_AXIS_xU16", [52, 55, 56, 64, 66], [52.0, 55.0, 56.0, 64.0, 66.0])
+
+
+def test_axis_pts002(offline):
+    load_save_verify_axis_pts(
+        offline,
+        "CDF20.axis.X_RE_AXIS_xS8",
+        [10, 2, 18, 3, 33, 4, 51, 5, 52, 6, 68, 7, 85, 8],
+        [10.0, 2.0, 18.0, 3.0, 33.0, 4.0, 51.0, 5.0, 52.0, 6.0, 68.0, 7.0, 85.0, 8.0],
+    )
+
+
+def test_axis_pts003(offline):
+    load_save_verify_axis_pts(
+        offline,
+        "LUT1D_1_x_table",
+        [156, 166, 176, 186, 196, 206, 216, 226, 236, 246, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+        [
+            1.21875,
+            1.296875,
+            1.375,
+            1.453125,
+            1.53125,
+            1.609375,
+            1.6875,
+            1.765625,
+            1.84375,
+            1.921875,
+            0.0,
+            0.078125,
+            0.15625,
+            0.234375,
+            0.3125,
+            0.390625,
+            0.46875,
+            0.546875,
+            0.625,
+            0.703125,
+            0.78125,
+        ],
+    )
+
+
+def test_axis_pts004(offline):
+    load_save_verify_axis_pts(
+        offline,
+        "LUT2D_1_x_table",
+        [166, 186, 206, 226, 246, 10, 30, 50, 70, 90],
+        [1.296875, 1.453125, 1.609375, 1.765625, 1.921875, 0.078125, 0.234375, 0.390625, 0.546875, 0.703125],
+    )
+
+
+def test_axis_pts005(offline):
+    load_save_verify_axis_pts(offline, "LUT2D_1_y_table", [181, 231, 75], [1.4140625, 1.8046875, 0.5859375])
+
+
+def test_axis_pts006(offline):
+    # load_save_verify_axis_pts(offline, "Rec2Sine_x_table", [0, 11266, 22276, 33542, 44552, 55818, 1293, 12559, 23569,
+    # 34835, 46101, 57111, 2842, 13852, 25118, 36128, 47394, 58404, 4135], [0. ,7.07863657, 13.99642359, 21.07506016,
+    # 27.99284718, 35.07148375, 0.81241586, 7.89105243, 14.80883945, 21.88747602, 28.96611258, 35.88389961, 1.78568126,
+    # 8.70346829, 15.78210485, 22.69989188, 29.77852844, 36.69631547, 2.59809712])
+    pass
+
+
+def test_axis_pts0XX(offline):
+    # load_save_verify_axis_pts(offline, "CDF20.axis.X_AXIS_xU16", [], [])
+    pass
